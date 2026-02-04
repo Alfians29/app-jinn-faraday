7ffdf8f9c4ebf856e153b97ee9f8cb3bb7790819
Muhammad Alfian <me.alfians29@gmail.com>
2026-02-04 20:49:02 +0700

    feat: add Footer component with internationalized disclaimer and roleplay text.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className='w-full bg-black border-t border-yellow-500/20 py-4 md:py-8 px-4 md:px-10'>
       <div className='max-w-[1800px] mx-auto text-center'>
-        <p className='text-gray-600 font-[Inter] text-xs mt-2'>
+        <p className='text-gray-600 font-[Inter] text-xs'>
           {t('footer.disclaimer')}
         </p>
         <p className='text-gray-600 font-[Inter] text-xs mt-1'>
