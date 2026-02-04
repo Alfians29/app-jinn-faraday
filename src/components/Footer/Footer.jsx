47bc5fe9aee7c639c7d7d63f0c764379d74c0b48
Muhammad Alfian <me.alfians29@gmail.com>
2026-02-04 20:44:21 +0700

    feat: add Footer component with internationalized disclaimer and affiliation text.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,17 +7,6 @@ const Footer = () => {
   return (
     <footer className='w-full bg-black border-t border-yellow-500/20 py-4 md:py-8 px-4 md:px-10'>
       <div className='max-w-[1800px] mx-auto text-center'>
-        <p className='text-gray-500 font-[Inter] text-sm'>
-          Built with 🤘 by{' '}
-          <a
-            href='https://www.tiktok.com/@letsgoalfian'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-yellow-500 hover:text-yellow-400 transition-colors'
-          >
-            @alfiyyann
-          </a>
-        </p>
         <p className='text-gray-600 font-[Inter] text-xs mt-2'>
           {t('footer.disclaimer')}
         </p>
